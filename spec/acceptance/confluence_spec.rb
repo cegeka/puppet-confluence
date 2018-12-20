3d7f30f3493cc59a7fb4fd008b19834fa632d289
Stan Bogdan <bogdan.stan@cegeka.be>
2018-12-20 15:43:22 +0200

    [Puppet-module] Fix spec tests for confluence - first draft

---
+++ spec/acceptance/confluence_spec.rb
@@ -0,0 +1,40 @@
+  require 'spec_helper_acceptance'
+
+describe 'nginx' do
+
+  describe 'running puppet code' do
+    it 'should work with no errors' do
+      pp = <<-EOS
+
+      include 'yum'
+      include 'cegekarepos'
+      include 'profile::package_management'
+      Yum::Repo <| title == 'cegeka-unsigned' |>
+      sunjdk::instance { 'jdk-1.8.0_77-fcs':
+        ensure      => 'present',
+        jdk_version => '1.8.0_77-fcs'
+      }
+
+      class { 'confluence':
+        version      => '6.10.0',
+        checksum     => '6c982c7f4356e2f121022fc87dc70a45',
+        javahome     => '/usr/java/jdk1.8.0_77/',
+      }
+      class { 'confluence::facts': }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe port(443) do
+      it { is_expected.to be_listening }
+    end
+
+    describe service('confluence') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+  end
+end
