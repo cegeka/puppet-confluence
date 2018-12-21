f03a48d0bf375bbb71c2be5e35d1897eab28cbee
Stan Bogdan <32199254+stanbog@users.noreply.github.com>
2018-12-21 17:01:59 +0200

    Update confluence_spec.rb

--- spec/acceptance/confluence_spec.rb
+++ spec/acceptance/confluence_spec.rb
@@ -20,7 +20,7 @@ describe 'nginx' do
         checksum     => '6c982c7f4356e2f121022fc87dc70a45',
         javahome     => '/usr/java/jdk1.8.0_77/',
       }
-      class { 'confluence::facts': }
+   #   class { 'confluence::facts': }
       EOS
 
       # Run it twice and test for idempotency
