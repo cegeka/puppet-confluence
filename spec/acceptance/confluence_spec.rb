9916a7a9ee0489d5fa952789e938a3f5b56ce967
Stan Bogdan <32199254+stanbog@users.noreply.github.com>
2019-01-07 10:33:10 +0200

    Update confluence_spec.rb

--- spec/acceptance/confluence_spec.rb
+++ spec/acceptance/confluence_spec.rb
@@ -20,6 +20,7 @@ describe 'apache' do
         checksum     => '6c982c7f4356e2f121022fc87dc70a45',
         javahome     => '/usr/java/jdk1.8.0_05/',
       }
+      class { 'confluence::facts': }
       EOS
 
       # Run it twice and test for idempotency
