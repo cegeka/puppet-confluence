5299d5a32619f1a589105c282774c404b31c2a90
Stan Bogdan <32199254+stanbog@users.noreply.github.com>
2019-01-07 17:00:56 +0200

    Update confluence_spec.rb

--- spec/acceptance/confluence_spec.rb
+++ spec/acceptance/confluence_spec.rb
@@ -13,7 +13,7 @@ describe 'confluence' do
       sunjdk::instance { 'jdk-1.8.0_05-fcs':
         ensure      => 'present',
         jdk_version => '1.8.0_05-fcs',
-        pkg_name    => 'jdk1.8.0_05'
+        pkg_name    => 'jdk-1.8.0_05-fcs'
       }
 
       class { 'confluence':
