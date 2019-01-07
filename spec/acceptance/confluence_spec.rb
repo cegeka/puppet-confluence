596f0e02b3333ffd80b34db9ad4f87e05bedbeac
Stan Bogdan <32199254+stanbog@users.noreply.github.com>
2019-01-07 17:11:14 +0200

    Update confluence_spec.rb

--- spec/acceptance/confluence_spec.rb
+++ spec/acceptance/confluence_spec.rb
@@ -24,8 +24,8 @@ describe 'confluence' do
       EOS
 
       # Run it twice and test for idempotency
-#      apply_manifest(pp, :catch_failures => true)
-#      apply_manifest(pp, :catch_changes => true)
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
     end
 
     describe service('confluence') do
