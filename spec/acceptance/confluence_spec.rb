ba94790d61519f3189001714be5492643739fd30
Stan Bogdan <32199254+stanbog@users.noreply.github.com>
2019-01-08 10:05:54 +0200

    Update confluence_spec.rb

--- spec/acceptance/confluence_spec.rb
+++ spec/acceptance/confluence_spec.rb
@@ -17,7 +17,6 @@ describe 'confluence' do
       }
 
       class { 'confluence':
-        ensure       => 'running',
         version      => '6.10.0',
         checksum     => '6c982c7f4356e2f121022fc87dc70a45',
         javahome     => '/usr/java/jdk1.8.0_05/'
