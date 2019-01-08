80f7f1818cf9a5725b720d6fde7557abcd8c0c7c
Stan Bogdan <32199254+stanbog@users.noreply.github.com>
2019-01-08 09:15:43 +0200

    Update confluence_spec.rb

--- spec/acceptance/confluence_spec.rb
+++ spec/acceptance/confluence_spec.rb
@@ -10,16 +10,16 @@ describe 'confluence' do
       include 'cegekarepos'
       include 'profile::package_management'
       Yum::Repo <| title == 'cegeka-unsigned' |>
-      sunjdk::instance { 'jdk1.8.0_77-fcs':
+      sunjdk::instance { 'jdk-1.8.0_05-fcs':
         ensure      => 'present',
-        jdk_version => '1.8.0_77-fcs',
+        jdk_version => '1.8.0_05-fcs',
         require     => Yum::Repo['cegeka-unsigned']
       }
 
       class { 'confluence':
         version      => '6.10.0',
         checksum     => '6c982c7f4356e2f121022fc87dc70a45',
-        javahome     => '/usr/java/jdk1.8.0_77/'
+        javahome     => '/usr/java/jdk1.8.0_05/'
       }
       EOS
 
